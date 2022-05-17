6a31a9a355c3d441a02ad78b4f9cad35b68eba56
m-triple-m <triplem656@gmail.com>
2022-05-17 13:15:54 +0530

    ..

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const app_config = {
-  backend_url: "http://localhost:5000",
+  // backend_url: "http://localhost:5000",
+  backend_url: "https://myprojectrestapi.herokuapp.com/",
 };
 
 export default app_config;
